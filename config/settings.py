89c78ca0e3cdabfea52d2ea07c8ab2d760648ebd
Paulo Germano <>
2026-08-29 10:15:45 -0300

    chore: adiciona livros em settings.py

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'livros.apps.LivrosConfig'
 ]
 
 ROOT_URLCONF = 'config.urls'
--- livros/apps.py
+++ livros/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class LivrosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'livros'
+    verbose_name = 'Livros'
